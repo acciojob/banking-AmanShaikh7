e8138abc012769147b8a86db3c59097fd8a542a2
Aman <amanshaikh7102000@gmail.com>
2023-06-17 16:12:03 +0530

    Tested all classes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,11 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        BankAccount bankAccount = new BankAccount("Aman",10000,2000);
+        bankAccount.deposit(1000);
+        SavingsAccount savingsAccount = new SavingsAccount("Aman",12000,5000,1.2);
+        savingsAccount.withdraw(6000);
+        //bankAccount.generateAccountNumber(8,20);
     }
 }
